a6408deff7194d9912ac050489ac3995369703fa
TAK <infernal-slam@hotmail.com>
2016-04-05 00:59:35 +0700

    test

--- public/js/todo.js
+++ public/js/todo.js
@@ -1,8 +1,13 @@
-/* global angular*/
+/* global angular moment */
 angular.module('todoApp', [])
-  .controller('MyController', function ($scope, $http) {
-    $scope.title = 'Login Page Wifi'
-    $scope.login = function (input) {
+  .controller('MyController', function ($http, $interval) {
+    var app = this
+    app.title = 'Login Page Wifi'
+    $interval(function () {
+      app.time = moment(new Date()).format('LTS')
+    }, 10)
+    app.login = function (input) {
       console.log(input.username, input.password)
+    // save(input)
     }
   })
