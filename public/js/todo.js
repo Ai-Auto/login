8b52024ff924e21db94c654960093d62276039a7
TAK <infernal-slam@hotmail.com>
2016-04-05 00:12:01 +0700

    test

--- public/js/todo.js
+++ public/js/todo.js
@@ -1,5 +1,8 @@
 /* global angular*/
 angular.module('todoApp', [])
-  .controller('MyController', function ($scope) {
+  .controller('MyController', function ($scope, $http) {
     $scope.title = 'Login Page Wifi'
+    $scope.login = function (input) {
+      console.log(input.username, input.password)
+    }
   })
